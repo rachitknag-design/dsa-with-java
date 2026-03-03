b72540f081006bf81386e8c32293b99c19fe1885
Rachit Nag <rachitknag@gmail.com>
2026-03-03 13:31:54 +0530

    Kadane's Algorithm
    Solved but need to revisit
    TC : O(N)
    SC : O(1) :

---
+++ Arrays/MaximumSubarraySum.java
@@ -0,0 +1,34 @@
+import java.util.Scanner; 
+
+public class MaximumSubarraySum {
+
+    static int maxSumSubArray(int[] arr){
+       int maxSum = Integer.MIN_VALUE;
+       int sum =0 ;
+       for(int i=0;i<arr.length;i++){
+        sum=sum+arr[i];
+        if(sum>maxSum){
+            maxSum=sum;
+        }
+        if(sum<0){
+            sum=0;
+        }
+       }
+       return maxSum;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the array size: ");
+        int n = sc.nextInt();
+
+        System.out.println("ENter the array elements: ");
+        int[] arr = new int[n];
+        for(int i=0;i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println("Maximum Sum of an subArray: "+maxSumSubArray(arr));
+        sc.close();
+    }
+}
