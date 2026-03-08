30668e11c71b3f7d4ba2a73a630cf9e134062c01
Rachit Nag <rachitknag@gmail.com>
2026-03-08 18:35:02 +0530

    Solved Selection Sort Algorithm
    TC : O(N^N)
    SC : O(1)

--- SortingAlgorithms/SelectionSort.java
+++ SortingAlgorithms/SelectionSort.java
@@ -1,41 +1,42 @@
+import java.util.Scanner;
+
 public class SelectionSort {
 
-    public static void sort(int[] arr){
+    static void sort(int[] arr){
+
         int n = arr.length;
 
         for(int i=0;i<n-1;i++){
             int minIndex = i;
             for(int j=i+1;j<n;j++){
-                if(arr[j]<arr[minIndex]){
+                if(arr[minIndex]>arr[j]){
                     minIndex = j;
                 }
-                
             }
-
             int temp = arr[minIndex];
             arr[minIndex] = arr[i];
             arr[i] = temp;
         }
 
-        System.out.println("Array Elements are after sorting: ");
-        for(int val : arr){
-            System.out.print(val+" ");
+        System.out.println("Sorted array is: ");
+        for(int x: arr){
+            System.out.print(x+" ");
         }
     }
-
     public static void main(String[] args) {
-        int[] arr = {13, 46, 24, 52, 20, 9};
+        Scanner sc = new Scanner(System.in);
 
-        // Print array before sorting
-        System.out.println("Before selection sort:");
-        for (int num : arr) {
-            System.out.print(num + " ");
+        System.out.print("Enter the array size: ");
+        int n = sc.nextInt();
+
+        System.out.println("Enter the array elements: ");
+        int[] arr = new int[n];
+        for(int i=0;i<n;i++){
+            arr[i] = sc.nextInt();
         }
-        System.out.println();
 
-        // Call selection sort
         sort(arr);
 
+        sc.close();
     }
-    
 }
