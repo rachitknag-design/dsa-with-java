a5afaa399343f944531e1ac86e3b653bb746f497
Rachit Nag <rachitknag@gmail.com>
2026-03-10 08:36:02 +0530

    Solved
    TC :O(N2) for the worst and average cases and O(N) for the best case. Here, N = size of the array.
    SC :O(1)

--- SortingAlgorithms/BubbleSort.java
+++ SortingAlgorithms/BubbleSort.java
@@ -6,15 +6,18 @@ public class BubbleSort {
         int n = arr.length;
 
         for(int i=n-1;i>=0;i--){
-            int maxIndex = i;
-            for(int j=i-1;j>0;j++){
-                if(arr[maxIndex]<arr[j]){
-                    maxIndex = j;
+            boolean didSwap = false;
+            for(int j=0;j<=i-1;j++){
+                if(arr[j]>arr[j+1]){
+                    int temp = arr[j+1];
+                    arr[j+1] = arr[j];
+                    arr[j] = temp;
+                    didSwap = true;
                 }
             }
-            int temp = arr[maxIndex];
-            arr[maxIndex] = arr[i];
-            arr[i] = temp;
+            if(!didSwap){
+                break;
+            }
         }
 
         System.out.println("Array after sorting: ");
