ce4b5ad49617be90b183325cda925e53d3bc4f81
Rachit Nag <rachitknag@gmail.com>
2026-06-09 07:01:28 +0530

    Create PrintAlternateElementsofanArray.java

---
+++ arrays-basics/PrintAlternateElementsofanArray.java
@@ -0,0 +1,8 @@
+public class PrintAlternateElementsofanArray {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,6,7,8};
+        for(int i=0;i<arr.length;i=i+2){
+            System.out.println("["+i+","+arr[i]+"]");
+        }
+    }
+}
