e369d7a721ad86e12b63d94d7698615fafbdaa93
Rachit Nag <rachitknag@gmail.com>
2026-06-15 21:41:44 +0530

    Update DeleteanElementattheXthPosition.java
    Solved:
    TC : O(n)
    SC : O(n)

--- arrays-basics/DeleteanElementattheXthPosition.java
+++ arrays-basics/DeleteanElementattheXthPosition.java
@@ -1,3 +1,31 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class DeleteanElementattheXthPosition {
-    
+     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in); 
+        int arr[] = {10,20,40,50};
+        int newArr[]  = new int[arr.length-1];
+        System.out.print("Enter Index: ");
+        int index = sc.nextInt();
+        if(index<1||index>arr.length){
+            System.out.println("Invalid input");
+            sc.close();
+            return;
+        }
+
+        for(int i=0;i<index-1;i++){
+            newArr[i] = arr[i];
+        }
+
+        for(int i=index-1;i<newArr.length;i++){
+            newArr[i] = arr[i+1];
+        }
+        
+
+        System.out.println(Arrays.toString(newArr));
+
+        sc.close();
+
+    }    
 }
