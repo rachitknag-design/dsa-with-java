bb3121aa9d11f3ba454e2182340b2eb10e19aaf1
Rachit Nag <rachitknag@gmail.com>
2026-06-15 19:57:28 +0530

    Create FindtheSecondMaximumElement.java
    Solved:
    TC: O(n)
    SC: O(1)

---
+++ arrays-basics/FindtheSecondMaximumElement.java
@@ -0,0 +1,23 @@
+public class FindtheSecondMaximumElement {
+    public static void main(String[] args) {
+        int arr[] = {2,1,4,5,3,7};
+        int max = Integer.MIN_VALUE;
+        int second_max = Integer.MIN_VALUE;
+
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]>max){
+                second_max = max;
+                max = arr[i];
+            }else if(arr[i]>second_max&&arr[i]!=max){
+                second_max = arr[i];
+            }
+        }
+
+        if(second_max == Integer.MIN_VALUE){
+            System.out.println("Second maximum element not found");
+            return;
+        }else{
+            System.out.println("Second maximum element : "+second_max);
+        }
+    }
+}
