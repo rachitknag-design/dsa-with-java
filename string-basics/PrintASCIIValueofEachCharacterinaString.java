ffd803e19f38b754dcdce6b3b82be0cb69209bcc
Rachit Nag <rachitknag@gmail.com>
2026-07-03 00:45:10 +0530

    Update PrintASCIIValueofEachCharacterinaString.java
    Solved

--- string-basics/PrintASCIIValueofEachCharacterinaString.java
+++ string-basics/PrintASCIIValueofEachCharacterinaString.java
@@ -1,3 +1,8 @@
 public class PrintASCIIValueofEachCharacterinaString {
-    
+    public static void main(String[] args) {
+        String str = "Welcome to Java!!";
+        for(int i=0;i<str.length();i++){
+            System.out.print((int)str.charAt(i)+" ");
+        }
+    }
 }
