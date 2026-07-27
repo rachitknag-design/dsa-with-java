e601629d9f62a4397228a79ffa499101e6a042e1
Rachit Nag <rachitknag@gmail.com>
2026-07-27 11:05:13 +0530

    Update Program1.java

--- 1D-LinkedList/Program1.java
+++ 1D-LinkedList/Program1.java
@@ -1,3 +1,40 @@
+class Node {
+    int data;
+    Node next;
+
+    public Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+
+
+}
+
 public class Program1 {
     
+    public static void main(String[] args) {
+        Node head = new Node(10);
+
+        Node second = new Node(20);
+        head.next = second;
+
+        Node third = new Node(30);
+        second.next = third;
+
+        System.out.println("The linkedList elements are:"); 
+        print(head);
+    }
+
+    private static void print(Node head) {
+        if(head==null){
+            return;
+        }
+
+        Node temp = head;
+
+        while(temp!=null) {
+            System.out.print(temp.data+" ");
+            temp=temp.next;
+        }
+    }
 }
